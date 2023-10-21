d84422c537e4da7283876a2db2bad7aaf3389045
Victor Ranzani <ranzani@unifei.edu.br>
2023-10-21 02:38:13 -0300

    Implementada sequência de bits
    Frequências para números de 4 bits funcionando devidamente e foi adicionada uma função para colocar 3 números de 4 bits em sequencia para gerar um sinal final

--- bitaudio.py
+++ bitaudio.py
@@ -1,104 +1,44 @@
-'''
-import pyaudio
-import numpy as np
-import matplotlib.pyplot as plt
-import tkinter as tk
-from tkinter import messagebox
-
-# Parameters
-sampling_rate = 44100  # Number of samples per second
-
-# Create the main window
-window = tk.Tk()
-window.title("Bit to Frequency Converter")
-
-# Create the entry fields
-total_duration_label = tk.Label(window, text="Total Duration (seconds):")
-total_duration_label.pack()
-total_duration_entry = tk.Entry(window)
-total_duration_entry.pack()
-
-bit_sequence_label = tk.Label(window, text="Enter 4-bit sequence:")
-bit_sequence_label.pack()
-bit_sequence_entry = tk.Entry(window)
-bit_sequence_entry.pack()
-
-def generate_bit_signal(total_duration):
-    bit_sequence = bit_sequence_entry.get()
-
-    # Check if the bit sequence is valid
-    if len(bit_sequence) != 4 or not all(bit in ['0', '1'] for bit in bit_sequence):
-        messagebox.showerror("Error", "Invalid bit sequence. Please enter a valid 4-bit sequence.")
-        return
-
-    initial_frequency = 1000 + int(bit_sequence, 2) * (1000 / 16)
-    final_frequency = initial_frequency + 1000
-
-    tone_duration = total_duration / 2
-    num_samples = int(sampling_rate * tone_duration)
-    t = np.linspace(0, tone_duration, num_samples, endpoint=False)
-
-    # Use a chirp function to create a smoothly varying frequency signal
-    frequency_signal = np.sin(2 * np.pi * np.linspace(initial_frequency, final_frequency, num_samples) * t)
-
-    return frequency_signal
-def play_bit_audio():
-    total_duration = float(total_duration_entry.get())
-    signal = generate_bit_signal(total_duration)
-
-    if signal is not None:
-        # Play the audio signal
-        p = pyaudio.PyAudio()
-
-        stream = p.open(format=pyaudio.paFloat32,
-                        channels=1,
-                        rate=sampling_rate,
-                        output=True)
-
-        stream.write(signal.astype(np.float32).tobytes())
-
-        stream.stop_stream()
-        stream.close()
-
-        p.terminate()
-
-
-def plot_bit_graph():
-    total_duration = float(total_duration_entry.get())
-    signal = generate_bit_signal(total_duration)
-
-    if signal is not None:
-        # Compute the spectrogram
-        spec, freqs, t, im = plt.specgram(signal, NFFT=1024, Fs=sampling_rate, noverlap=512, cmap='jet')
-
-        # Set the axis labels
-        plt.xlabel('Time')
-        plt.ylabel('Frequency')
-
-        # Adjust the y-axis limits
-        plt.ylim(0, 3300)  # Adjust these values to focus on the range of frequencies used
-
-        # Set the colorbar
-        plt.colorbar()
-
-        # Display the plot
-        plt.show()
-
-
-# Create the buttons
-play_button = tk.Button(window, text="Play Bit Audio", command=play_bit_audio)
-play_button.pack()
-
-plot_button = tk.Button(window, text="Plot Bit Graph", command=plot_bit_graph)
-plot_button.pack()
-
-# Run the main window loop
-window.mainloop()
-'''
-import numpy as np
+'''import numpy as np
 import matplotlib.pyplot as plt
 from tkinter import *
 import pyaudio
+
+# Function to generate the Chirp Spread Spectrum (CSS) signal
+def generate_css(bit_number, duration_seconds):
+    base_freq = 1000 + (bit_number / 16) * 1000
+    sample_rate = 44100
+    time = np.linspace(0, duration_seconds, int(sample_rate * duration_seconds))
+    chirp_rate = 1000  # Chirp rate in Hz per second
+
+    css_signal = np.sin(2 * np.pi * (base_freq * time + 0.5 * chirp_rate * time ** 2))
+
+    return css_signal, time
+
+# Function to play the CSS signal
+def play_css(bit_number, duration_seconds):
+    css_signal, _ = generate_css(bit_number, duration_seconds)
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=44100,
+                    output=True)
+
+    stream.write(css_signal.astype(np.float32).tobytes())
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+# Function to plot the CSS signal
+def plot_waterfall_css(bit_number, duration_seconds):
+    css_signal, _ = generate_css(bit_number, duration_seconds)
+
+    plt.specgram(css_signal, Fs=44100, NFFT=1024, noverlap=900, cmap='viridis')
+    plt.title('Waterfall Plot of Chirp Spread Spectrum Signal')
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    plt.show()
+
 
 # Function to generate the tone
 def generate_tone(bit_number, duration_seconds):
@@ -113,11 +53,14 @@ def generate_tone(bit_number, duration_seconds):
 
     tone = np.sin(2 * np.pi * frequency_array * time)
 
-    return tone, time
+    return tone, time, frequency_array
+
+# Function to apply CSS modulation
 
 # Function to play the tone
-def play_tone(bit_number, duration_seconds):
-    tone, _ = generate_tone(bit_number, duration_seconds)
+def play_tone(bit_number, duration_seconds, css_modulation=False):
+    tone, _, _ = generate_tone(bit_number, duration_seconds)
+
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paFloat32,
                     channels=1,
@@ -129,18 +72,26 @@ def play_tone(bit_number, duration_seconds):
     stream.close()
     p.terminate()
 
-# Function to plot the signal
-def plot_waterfall(bit_number, duration_seconds):
-    tone, time = generate_tone(bit_number, duration_seconds)
+# Function to plot the waterfall plot
+def plot_waterfall(bit_number, duration_seconds, css_modulation=False):
+    tone, time, _ = generate_tone(bit_number, duration_seconds)
+
     plt.specgram(tone, Fs=44100, NFFT=1024, noverlap=900, cmap='viridis')
     plt.title('Waterfall Plot of Frequency over Time')
     plt.xlabel('Time')
     plt.ylabel('Frequency')
     plt.show()
 
-# Create the UI
-root = Tk()
-root.title("Tone Generator")
+# Function to plot the frequency over time
+def plot_frequency_time(bit_number, duration_seconds, css_modulation=False):
+    _, time, frequency_array = generate_tone(bit_number, duration_seconds)
+
+    plt.plot(time, frequency_array)
+    plt.title('Frequency over Time')
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    plt.show()
+
 
 # Function to get the input and generate the tone
 def get_input():
@@ -154,6 +105,23 @@ def plot():
     duration_seconds = float(entry_duration.get())
     plot_waterfall(bit_number, duration_seconds)
 
+
+# Function to plot the frequency over time
+def plot_freq_time():
+    bit_number = int(entry.get(), 2)
+    duration_seconds = float(entry_duration.get())
+    plot_frequency_time(bit_number, duration_seconds)
+
+def modulate_css():
+    bit_number = int(entry.get(), 2)
+    duration_seconds = float(entry_duration.get())
+    play_css(bit_number, duration_seconds)
+    plot_waterfall_css(bit_number, duration_seconds)
+
+# Create the UI
+root = Tk()
+root.title("Tone Generator")
+
 label = Label(root, text="Enter 4-bit number:")
 label.pack()
 
@@ -172,7 +140,112 @@ button_tone.pack()
 button_plot = Button(root, text="Plot Waterfall", command=plot)
 button_plot.pack()
 
+button_freq_time = Button(root, text="Plot Frequency over Time", command=plot_freq_time)
+button_freq_time.pack()
+
+# Create the new button for CSS modulation
+button_css = Button(root, text="Modulate to CSS", command=modulate_css)
+button_css.pack()
+
 root.mainloop()
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tkinter import *
+import pyaudio
+
+# Function to generate the Chirp Spread Spectrum (CSS) signal
+def generate_css(bit_number, duration_seconds):
+    base_freq = 1000 + (bit_number / 16) * 1000
+    sample_rate = 44100
+    time = np.linspace(0, duration_seconds, int(sample_rate * duration_seconds))
+    chirp_rate = 1000  # Chirp rate in Hz per second
+
+    css_signal = np.sin(2 * np.pi * (base_freq * time + 0.5 * chirp_rate * time ** 2))
+
+    return css_signal, time
+
+# Function to play the CSS signal
+def play_css(bit_number, duration_seconds):
+    css_signal, _ = generate_css(bit_number, duration_seconds)
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=44100,
+                    output=True)
+
+    stream.write(css_signal.astype(np.float32).tobytes())
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+# Function to plot the CSS signal
+def plot_waterfall_css(bit_number, duration_seconds):
+    css_signal, _ = generate_css(bit_number, duration_seconds)
+
+    plt.specgram(css_signal, Fs=44100, NFFT=1024, noverlap=900, cmap='viridis')
+    plt.title('Waterfall Plot of Chirp Spread Spectrum Signal')
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+# Function to generate the tone
+def generate_tone(bit_number, duration_seconds):
+    base_freq = 1000 + (bit_number / 16) * 1000
+    sample_rate = 44100
+    time = np.linspace(0, duration_seconds, int(sample_rate * duration_seconds))
+    frequency_array = np.linspace(base_freq, base_freq + 1000, len(time))
+
+    for i in range(len(frequency_array)):
+        while frequency_array[i] > 2000:
+            frequency_array[i] -= 1000
+
+    tone = np.sin(2 * np.pi * frequency_array * time)
+
+    return tone, time, frequency_array
+
+# Function to apply CSS modulation
+
+# Function to play the tone
+def play_tone(bit_number, duration_seconds, css_modulation=False):
+    tone, _, _ = generate_tone(bit_number, duration_seconds)
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=44100,
+                    output=True)
+
+    stream.write(tone.astype(np.float32).tobytes())
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+# Function to plot the waterfall plot
+def plot_waterfall(bit_number, duration_seconds, css_modulation=False):
+    tone, time, _ = generate_tone(bit_number, duration_seconds)
+
+    plt.specgram(tone, Fs=44100, NFFT=1024, noverlap=900, cmap='viridis')
+    plt.title('Waterfall Plot of Frequency over Time')
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    plt.show()
+
+# Function to plot the frequency over time
+def plot_frequency_time(bit_number, duration_seconds, css_modulation=False):
+    _, time, frequency_array = generate_tone(bit_number, duration_seconds)
+
+    plt.plot(time, frequency_array)
+    plt.title('Frequency over Time')
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+# Function to get the input and generate the tone
 def get_input():
     bit_number = int(entry.get(), 2)
     duration_seconds = float(entry_duration.get())
@@ -183,12 +256,65 @@ def plot():
     bit_number = int(entry.get(), 2)
     duration_seconds = float(entry_duration.get())
     plot_waterfall(bit_number, duration_seconds)
+
+
+# Function to plot the frequency over time
+def plot_freq_time():
+    bit_number = int(entry.get(), 2)
+    duration_seconds = float(entry_duration.get())
+    plot_frequency_time(bit_number, duration_seconds)
+
+def modulate_css():
+    bit_number = int(entry.get(), 2)
+    duration_seconds = float(entry_duration.get())
+    play_css(bit_number, duration_seconds)
+    plot_waterfall_css(bit_number, duration_seconds)
+
+# Function to get the input sequence and generate the concatenated CSS modulated signal
+def get_sequence():
+    sequence = entry_sequence.get()
+    if len(sequence) == 12:
+        concatenated_signal = np.array([])
+        for i in range(0, len(sequence), 4):
+            bit_number = int(sequence[i:i+4], 2)
+            css_signal, _ = generate_css(bit_number, 1)
+            if i == 0:
+                concatenated_signal = css_signal
+            else:
+                concatenated_signal = np.concatenate((concatenated_signal, css_signal), axis=None)
+        play_concatenated_signal(concatenated_signal)
+        plot_waterfall_concatenated(concatenated_signal)
+    else:
+        print("Please enter a valid sequence of 3 four-bit numbers.")
+
+# Function to play the concatenated CSS modulated signal
+def play_concatenated_signal(concatenated_signal):
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=44100,
+                    output=True)
+    stream.write(concatenated_signal.astype(np.float32).tobytes())
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+def plot_waterfall_concatenated(concatenated_signal):
+    plt.specgram(concatenated_signal, Fs=44100, NFFT=1024, noverlap=900, cmap='viridis')
+    plt.title('Waterfall Plot of Concatenated CSS Modulated Signal')
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    plt.show()
+
+# Create the UI
+root = Tk()
+root.title("Tone Generator")
+
 label = Label(root, text="Enter 4-bit number:")
 label.pack()
 
 entry = Entry(root, width=10)
 entry.pack()
-
 
 label_duration = Label(root, text="Enter duration (in seconds):")
 label_duration.pack()
@@ -196,11 +322,28 @@ label_duration.pack()
 entry_duration = Entry(root, width=10)
 entry_duration.pack()
 
-
 button_tone = Button(root, text="Play Tone", command=get_input)
 button_tone.pack()
 
 button_plot = Button(root, text="Plot Waterfall", command=plot)
 button_plot.pack()
+
+button_freq_time = Button(root, text="Plot Frequency over Time", command=plot_freq_time)
+button_freq_time.pack()
+
+# Create the new button for CSS modulation
+button_css = Button(root, text="Modulate to CSS", command=modulate_css)
+button_css.pack()
+
+# Add a label and entry for the sequence of 4-bit numbers
+label_sequence = Label(root, text="Enter 3 four-bit numbers sequence:")
+label_sequence.pack()
+
+entry_sequence = Entry(root, width=20)
+entry_sequence.pack()
+
+# Add a button to modulate the sequence to CSS, concatenate, play, and plot the concatenated signal
+button_modulate_sequence = Button(root, text="Modulate Sequence to CSS", command=get_sequence)
+button_modulate_sequence.pack()
 
 root.mainloop()
